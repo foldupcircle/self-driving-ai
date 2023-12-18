01e78962cc984d7f72089b7dc939028ca0bc2e2c
Sricharan Guddanti <sricharanguddanti@berkeley.edu>
2023-12-17 20:55:37 -0800

    done with car mechanics

--- car.js
+++ car.js
@@ -10,52 +10,49 @@ class Car {
         this.acceleration = 0.2;
         this.max_speed = 3;
         this.friction = 0.05;
+        this.angle = 0;
     }
 
     draw(context) {
+        context.save();
+        context.translate(this.x, this.y);
+        context.rotate(-this.angle);
         context.beginPath();
+
         context.rect(
-            this.x - this.width/2,
-            this.y - this.height/2,
+            -this.width/2,
+            -this.height/2,
             this.width,
             this.height
         );
         context.fill();
+        context.restore();
     }
 
     update() {
         // Keyboard control updates
-        if (this.controls.forward) {
-            this.speed += this.acceleration;
-        }
-        if (this.controls.reverse) {
-            this.speed -= this.acceleration;
-        }
+        if (this.controls.forward) { this.speed += this.acceleration; }
+        if (this.controls.reverse) { this.speed -= this.acceleration; }
 
         // Max Speed Constraints
-        if (this.speed > this.max_speed) {
-            this.speed = this.max_speed;
-        }
-        if (this.speed < -this.max_speed/2) {
-            this.speed = -this.max_speed/2;
-        }
+        if (this.speed > this.max_speed) { this.speed = this.max_speed; }
+        if (this.speed < -this.max_speed/2) { this.speed = -this.max_speed/2; }
 
         // Friction
-        if (this.speed > 0) {
-            this.speed -= this.friction;
-        }
-        if (this.speed < 0) {
-            this.speed += this.friction;
-        }
+        if (this.speed > 0) { this.speed -= this.friction; }
+        if (this.speed < 0) { this.speed += this.friction; }
 
         // Edge Case for when the Car stops
-        if (Math.abs(this.speed) < this.friction) {
-            this.speed = 0;
-        }
+        if (Math.abs(this.speed) < this.friction) { this.speed = 0; }
 
-        
+        // Angle Movement
+        const reverse = this.speed > 0 ? 1 : -1;
+        if (this.controls.left) { this.angle += 0.03 * reverse; }
+        if (this.controls.right) { this.angle -= 0.03 * reverse; }
 
-        // Position Update
-        this.y -= this.speed;
+        // Position Update Based on Unit Circle
+        this.y -= Math.cos(this.angle) * this.speed;
+        this.x -= Math.sin(this.angle) * this.speed;
+
     }
 }
