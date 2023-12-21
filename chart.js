58626c067f4277f2e2a30371cc3b9e536f4eb1cf
Sricharan Guddanti <sriguddanti@gmail.com>
2023-12-20 20:46:53 -0800

    working on chart

---
+++ chart.js
@@ -0,0 +1,15 @@
+class Chart {
+    /* Data:
+    - xvalues: run number [1, 2, ...]
+    - y values: y pos advanced
+    */
+    static drawChart(chartContext, data) {
+        const xvalues = 
+        const chart = new Chart("myChart", {
+            type: 'line',
+            data: {
+                labels: 
+            }
+        });
+    }
+}
