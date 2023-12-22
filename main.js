10fa28da951f5aba4f78cce0917c5c03f98ad327
Sricharan Guddanti <sriguddanti@gmail.com>
2023-12-21 18:45:48 -0800

    graph done, improved nn

--- main.js
+++ main.js
@@ -15,13 +15,19 @@ const road = new Road(canvas.width / 2, canvas.width * 0.9);
 const generatedCars = 500;
 const cars = generateCars(generatedCars);
 const carYStart = 100;
+let runNum = 1;
 
 // Getting stored data from previous run if it exists, or else initalize to inital value
-let runData = {x: [1], y: [0]};
+let runData = {x: [], y: []};
 if (localStorage.getItem("runData")) {
     runData = JSON.parse(localStorage.getItem("runData"));
 }
-const damagedDistanceThreshold = 100.0; // Must be a float
+let lastRunScore = 0;
+if (localStorage.getItem("lastRunScore")) {
+    lastRunScore = JSON.parse(localStorage.getItem("lastRunScore"));
+}
+
+const damagedDistanceThreshold = 150.0; // Must be a float
 let play = true;
 let optimalCar = cars[0];
 let bestScore = 0;
@@ -39,38 +45,38 @@ if (localStorage.getItem("bestCar")) {
 const carTraffic = [
     new Car(road.getLaneCenter(1), -100, 30, 50, "TRAFFIC", 0),
     new Car(road.getLaneCenter(0), -300, 30, 50, "TRAFFIC", 0),
-    new Car(road.getLaneCenter(2), -300, 30, 50, "TRAFFIC", 0),
-    new Car(road.getLaneCenter(0), -500, 30, 50, "TRAFFIC", 0),
-    new Car(road.getLaneCenter(1), -500, 30, 50, "TRAFFIC", 0),
-    new Car(road.getLaneCenter(1), -700, 30, 50, "TRAFFIC", 0),
-    new Car(road.getLaneCenter(2), -700, 30, 50, "TRAFFIC", 0),
+    new Car(road.getLaneCenter(2), -500, 30, 50, "TRAFFIC", 0),
+    new Car(road.getLaneCenter(0), -700, 30, 50, "TRAFFIC", 0),
     new Car(road.getLaneCenter(1), -900, 30, 50, "TRAFFIC", 0),
-    new Car(road.getLaneCenter(0), -1100, 30, 50, "TRAFFIC", 0),
+    new Car(road.getLaneCenter(1), -900, 30, 50, "TRAFFIC", 0),
     new Car(road.getLaneCenter(2), -1100, 30, 50, "TRAFFIC", 0),
-    new Car(road.getLaneCenter(0), -1300, 30, 50, "TRAFFIC", 0),
     new Car(road.getLaneCenter(1), -1300, 30, 50, "TRAFFIC", 0),
+    new Car(road.getLaneCenter(0), -1300, 30, 50, "TRAFFIC", 0),
     new Car(road.getLaneCenter(2), -1500, 30, 50, "TRAFFIC", 0),
-    new Car(road.getLaneCenter(0), -1700, 30, 50, "TRAFFIC", 0),
-    new Car(road.getLaneCenter(1), -1900, 30, 50, "TRAFFIC", 0),
+    new Car(road.getLaneCenter(0), -1500, 30, 50, "TRAFFIC", 0),
+    new Car(road.getLaneCenter(1), -1700, 30, 50, "TRAFFIC", 0),
     new Car(road.getLaneCenter(2), -1900, 30, 50, "TRAFFIC", 0),
-    new Car(road.getLaneCenter(0), -2100, 30, 50, "TRAFFIC", 0),
+    new Car(road.getLaneCenter(0), -1900, 30, 50, "TRAFFIC", 0),
+    new Car(road.getLaneCenter(1), -2100, 30, 50, "TRAFFIC", 0),
+    new Car(road.getLaneCenter(2), -2100, 30, 50, "TRAFFIC", 0),
     new Car(road.getLaneCenter(0), -2300, 30, 50, "TRAFFIC", 0),
-    new Car(road.getLaneCenter(1), -2300, 30, 50, "TRAFFIC", 0),
+    new Car(road.getLaneCenter(0), -2500, 30, 50, "TRAFFIC", 0),
     new Car(road.getLaneCenter(1), -2500, 30, 50, "TRAFFIC", 0),
-    new Car(road.getLaneCenter(2), -2700, 30, 50, "TRAFFIC", 0),
     new Car(road.getLaneCenter(1), -2700, 30, 50, "TRAFFIC", 0),
-    new Car(road.getLaneCenter(0), -2900, 30, 50, "TRAFFIC", 0),
+    new Car(road.getLaneCenter(2), -2700, 30, 50, "TRAFFIC", 0),
     new Car(road.getLaneCenter(1), -2900, 30, 50, "TRAFFIC", 0),
-    new Car(road.getLaneCenter(2), -3100, 30, 50, "TRAFFIC", 0),
+    new Car(road.getLaneCenter(0), -2900, 30, 50, "TRAFFIC", 0),
     new Car(road.getLaneCenter(1), -3100, 30, 50, "TRAFFIC", 0),
-    new Car(road.getLaneCenter(0), -3300, 30, 50, "TRAFFIC", 0),
+    new Car(road.getLaneCenter(2), -3300, 30, 50, "TRAFFIC", 0),
+    new Car(road.getLaneCenter(1), -3300, 30, 50, "TRAFFIC", 0),
+    new Car(road.getLaneCenter(0), -3500, 30, 50, "TRAFFIC", 0),
     new Car(road.getLaneCenter(2), -3500, 30, 50, "TRAFFIC", 0),
     new Car(road.getLaneCenter(0), -3700, 30, 50, "TRAFFIC", 0),
     new Car(road.getLaneCenter(1), -3900, 30, 50, "TRAFFIC", 0)
 ]
 
 // Let the action begin...
-drawChart(runData);
+if (lastRunScore == 0) { drawChart(runData); }
 animate();
 
 function saveBestCar() {
@@ -83,18 +89,14 @@ function saveBestCar() {
     if (!optimalCar.finished) { location.reload(); }
     else { play = false; }
 }
-/* Basic Logic: Need to find a way to count a run everytime a car is on the start line
-- if (car.y == carYStart) {
 
-}
-
-*/
-
-function saveLastRun() {
-    if (optimalCar.y == carYStart) {
-        runData.y.push(JSON.parse(localStorage.getItem("lastRunScore")));
+function saveLastRun(runData) {
+    if (lastRunScore != 0 && optimalCar.y == carYStart) {
+        console.log(runData);
+        runData.y.push(JSON.parse(lastRunScore));
         runData.x.push(runData.x.length + 1);
         localStorage.setItem("runData", JSON.stringify(runData));
+        drawChart(runData);
     }
 }
 
@@ -118,7 +120,7 @@ function startPause() {
 function generateCars(n) {
     const cars = [];
     for (let i = 1; i <= n; i++) {
-        cars.push(new Car(road.getLaneCenter(1), 100, 30, 50, "AI", 3));
+        cars.push(new Car(road.getLaneCenter(1), 100, 30, 50, "AI", 5));
     }
     return cars;
 }
@@ -129,13 +131,12 @@ function drawChart(runData) {
     const data = [{
         x: runData.x,
         y: runData.y,
-        mode: "lines",
-        type: "scatter"
+        mode: 'lines + markers'
     }];
 
     // Define Layout
     const layout = {
-        xaxis: {range: [1, runData.x.length], title: "Number of Training Runs"},
+        xaxis: {range: [1, runData.x.length + 1], title: "Number of Training Runs"},
         yaxis: {range: [0, 4200], title: "Distance Covered"},
         title: "Y Distance Covered (Finish Line = 4200) vs # of Training Runs"
     };
@@ -153,7 +154,7 @@ function animate(time) {
         cars[i].update(road.borders, road.finishLine, carTraffic);
     }
     optimalCar = cars.find(c => c.y == (Math.min(...cars.map(c => c.y))) );
-    saveLastRun();
+    saveLastRun(runData);
 
     canvas.height = window.innerHeight;
     netCanvas.height = window.innerHeight;
@@ -182,7 +183,6 @@ function animate(time) {
     }
 
     if ((optimalCar.damaged && !notDamagedCarsWithinThreshold) || optimalCar.finished) {
-        console.log(optimalCar.damaged);
         saveBestCar();
     }
 
